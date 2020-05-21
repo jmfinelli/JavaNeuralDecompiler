8e657b573fe8343fd8467bdc2429f3b70560b7d5
manuNCL <j.finelli2@newcastle.ac.uk>
2020-05-21 19:27:51 +0100

    Ready To Use

---
+++ utilityFiles/createDatasets.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv("./datasets/pairs.output", sep='\t', header=None, names=['source', 'target'])
+
+df = df.sample(frac=1).reset_index(drop=True).sample(50000)
+
+x = df.source;
+y = df.target;
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.05)
+
+x_train.to_csv('./datasets/x_train', sep='\n', index=False)
+x_test.to_csv('./datasets/x_valid', sep='\n', index=False)
+y_train.to_csv('./datasets/y_train', sep='\n', index=False)
+y_test.to_csv('./datasets/y_valid', sep='\n', index=False)
