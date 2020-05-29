78fda4dabd09e320d091ba34a7fd03890d4088dc
manuNCL <j.finelli2@newcastle.ac.uk>
2020-05-29 17:44:49 +0100

    modified createDatasets

--- utilityFiles/createDatasets.py
+++ utilityFiles/createDatasets.py
@@ -3,14 +3,18 @@ from sklearn.model_selection import train_test_split
 
 df = pd.read_csv("./datasets/pairs.output", sep='\t', header=None, names=['source', 'target'])
 
-df = df.sample(frac=1).reset_index(drop=True).sample(50000)
+df_samples = df.sample(frac=1).reset_index(drop=True).sample(50000)
 
-x = df.source;
-y = df.target;
+df_valid = df.merge(df_samples, indicator=True, how='left')\
+    .query('_merge=="left_only"')\
+    .drop('_merge', axis=1)
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.05)
+x_train, x_test, y_train, y_test = train_test_split(df_samples.source, df_samples.target, test_size=0.05)
 
 x_train.to_csv('./datasets/x_train', sep='\n', header=False, index=False)
 x_test.to_csv('./datasets/x_valid', sep='\n', header=False, index=False)
 y_train.to_csv('./datasets/y_train', sep='\n', header=False, index=False)
 y_test.to_csv('./datasets/y_valid', sep='\n', header=False, index=False)
+
+df_valid['source'].to_csv('./datasets/remaining_sources', sep='\n', header=False, index=False)
+df_valid['target'].to_csv('./datasets/remaining_targets', sep='\n', header=False, index=False)
